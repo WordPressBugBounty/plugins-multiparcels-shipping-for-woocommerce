dfa8f69fcd71fdbbd9bc44530a66ec9c83ab99fd
Sean Murphy <sean@iamseanmurphy.com>
2024-11-20 09:28:44 +0000

    1.19.6

---
+++ multiparcels-shipping-for-woocommerce/languages/multiparcels-shipping-for-woocommerce-lv.l10n.php
@@ -0,0 +1,2 @@
+<?php
+return ['project-id-version'=>'MultiParcels Shipping For WooCommerce','report-msgid-bugs-to'=>'','pot-creation-date'=>'2024-11-20 08:35+0000','po-revision-date'=>'2024-11-20 08:35+0000','last-translator'=>'','language-team'=>'Latvian','language'=>'lv','plural-forms'=>'nplurals=3; plural=n%10==0||( n%100 >= 11 && n%100<=19)? 0 :(n%10==1 && n%100!=11 ? 1 : 2);','mime-version'=>'1.0','content-type'=>'text/plain; charset=UTF-8','content-transfer-encoding'=>'8bit','x-generator'=>'Loco https://localise.biz/','x-loco-version'=>'2.6.11; wp-6.6.2','messages'=>[]];
